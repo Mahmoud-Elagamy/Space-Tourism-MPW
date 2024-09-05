4d659960e4a936e1bcb1640a1a72bd12fa493d01
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-09-05 22:07:26 +0300

    add metadata

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,14 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Head from "next/head";
 import { Result, Button } from "antd";
 
-const NotFoundPage = () => {
+export const metadata = {
+  title: "404 - Page Not Found",
+  description: "Oops! The page you are looking for does not exist.",
+};
+
+const NotFound = () => {
   const router = useRouter();
   return (
     <div className="absolute inset-0 flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-br from-[#0B0D19] to-[#0C1D39] p-4 text-center">
@@ -19,4 +25,4 @@ const NotFoundPage = () => {
   );
 };
 
-export default NotFoundPage;
+export default NotFound;
