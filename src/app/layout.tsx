27b9d1b58847636e5f36ac4c8dceb4a9ee493c93
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-09-05 15:24:47 +0300

    added open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import type { Metadata } from "next";
 import { Bellefair, Barlow_Condensed } from "next/font/google";
 import "./globals.css";
@@ -34,6 +35,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${bellefair.variable} ${barlow.variable}`}>
+      <Head>
+        {/* <!-- Open Graph --> */}
+        <meta property="og:title" content="Space Tourism Website" />
+        <meta
+          property="og:description"
+          content="A minimal Space Tourism MPW built with Next.js."
+        />
+        <meta
+          property="og:image"
+          content="https://space-tourism-mpw.netlify.app/assets/Space-Tourism-preview.png"
+        />
+        <meta property="og:image:width" content="369" />
+        <meta property="og:image:height" content="492" />
+        <meta
+          property="og:url"
+          content="https://space-tourism-mpw.netlify.app/"
+        />
+        <meta property="og:type" content="website" />
+
+        {/* <!-- Twitter Card --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Space Tourism Website" />
+        <meta
+          name="twitter:description"
+          content="A minimal Space Tourism MPW built with Next.js."
+        />
+        <meta
+          name="twitter:image"
+          content="https://space-tourism-mpw.netlify.app/assets/Space-Tourism-preview.png"
+        />
+
+        <meta name="keywords" content="Space Tourism Website" />
+      </Head>
       <body className="bg-[#0B0D17]">
         <Header />
         {children}
