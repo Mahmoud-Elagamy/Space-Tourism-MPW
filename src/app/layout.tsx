520fa971c33f2b18529d60b2ddfc59b70ef863d8
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-09-05 15:42:26 +0300

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,22 @@ export const metadata: Metadata = {
     template: "%s | Space Tourism",
   },
   description: "A minimal Space Tourism MPW built with Next.js",
+  openGraph: {
+    title: "Space Tourism",
+    description: "A minimal Space Tourism MPW built with Next.js",
+    url: "https://space-tourism-mpw.netlify.app/",
+    images: [
+      {
+        url: "https://space-tourism-mpw.netlify.app/assets/Space-Tourism-preview.png",
+        width: 369,
+        height: 492,
+        alt: "Space-Tourism-preview",
+      },
+    ],
+    siteName: "Space-Tourism",
+    type: "website",
+  },
+  keywords: ["Space Tourism", "Website", "Next.js"],
 };
 
 export default function RootLayout({
@@ -35,39 +51,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${bellefair.variable} ${barlow.variable}`}>
-      <Head>
-        {/* <!-- Open Graph --> */}
-        <meta property="og:title" content="Space Tourism Website" />
-        <meta
-          property="og:description"
-          content="A minimal Space Tourism MPW built with Next.js."
-        />
-        <meta
-          property="og:image"
-          content="https://space-tourism-mpw.netlify.app/assets/Space-Tourism-preview.png"
-        />
-        <meta property="og:image:width" content="369" />
-        <meta property="og:image:height" content="492" />
-        <meta
-          property="og:url"
-          content="https://space-tourism-mpw.netlify.app/"
-        />
-        <meta property="og:type" content="website" />
-
-        {/* <!-- Twitter Card --> */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Space Tourism Website" />
-        <meta
-          name="twitter:description"
-          content="A minimal Space Tourism MPW built with Next.js."
-        />
-        <meta
-          name="twitter:image"
-          content="https://space-tourism-mpw.netlify.app/assets/Space-Tourism-preview.png"
-        />
-
-        <meta name="keywords" content="Space Tourism Website" />
-      </Head>
       <body className="bg-[#0B0D17]">
         <Header />
         {children}
