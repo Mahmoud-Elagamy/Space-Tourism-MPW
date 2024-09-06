fc5b40ea83fad7a1b2d8b23c23c83cdb77624988
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-09-06 18:00:37 +0300

    replace metadata object with regular js to set title & description

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,21 @@
 "use client";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Result, Button } from "antd";
 
-export const metadata = {
-  title: "404 - Page Not Found",
-  description: "Oops! The page you are looking for does not exist.",
-};
-
 const NotFound = () => {
   const router = useRouter();
+
+  useEffect(() => {
+    document.title = "404 - Page Not Found";
+    document
+      .querySelector("meta[name='description']")
+      ?.setAttribute(
+        "content",
+        "Oops! The page you are looking for does not exist.",
+      );
+  }, []);
+
   return (
     <div className="absolute inset-0 flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-br from-[#0B0D19] to-[#0C1D39] p-4 text-center">
       <Result
